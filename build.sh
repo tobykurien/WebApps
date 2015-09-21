5172d541e28910fce0a9f1765b97509c9b5015ff
Toby Kurien <toby@tobykurien.com>
2015-09-21 15:07:04 +0200

    Build script to make an unsigned release build (for F-droid)

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Make a release build. Using "gradle assembleRelease" fails the first time, so
+# this script runs the "prepare dependencies" task first.
+# See: https://github.com/tobykurien/WebApps/issues/20
+# NOTE: The release is unsigned, so you will need to manually sign it,
+# See: https://developer.android.com/tools/publishing/app-signing.html
+
+# run "gradle assembleDebug" to make a debug build rather (run it again if it fails
+# the first time).
+
+gradle prepareReleaseDependencies
+gradle assembleRelease
+
