dc60bd03829b74cb073e77e4653b25dfe36d5765
Toby Kurien <toby@tobykurien.com>
2020-03-31 13:05:22 +0200

    fix script

--- debug.sh
+++ debug.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./gradlew installDebug |grep "ERROR" && adb shell monkey -p com.tobykurien.webapps.debug -c android.intent.category.LAUNCHER 1
+./gradlew installDebug runApp |grep "ERROR"
