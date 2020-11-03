606f71f25d17380ba22ad0e17debfe24425ad26e
Toby Kurien <toby@tobykurien.com>
2020-11-03 15:31:39 +0200

    always clean to avoid weird issues

--- debug.sh
+++ debug.sh
@@ -5,7 +5,9 @@ else
   FILTER="$1"
 fi
 
-./gradlew installDebug runApp |grep "ERROR"
+echo Compiling app...
+# always clean because incremental compile sometimes fails and results in non-sensical compile errors
+./gradlew clean installDebug runApp |grep "ERROR"
 
 adb logcat -c
 adb logcat -v color -e "$FILTER" "*:D"
