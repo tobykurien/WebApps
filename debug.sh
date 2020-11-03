92e850193181ee586679aa7f4e2ea62431744d4e
Toby Kurien <toby@tobykurien.com>
2020-11-03 14:36:06 +0200

    add tailing of logcat with optional filter

--- debug.sh
+++ debug.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+if [ -e $1 ]; then
+  FILTER="com.tobykurien.webapps"
+else
+  FILTER="$1"
+fi
+
 ./gradlew installDebug runApp |grep "ERROR"
 adb logcat -c
+adb logcat -v color -e "$FILTER" "*:D"
